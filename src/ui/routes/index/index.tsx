9e316df5f51f618dead3ae5982cebbf2535b223c
Kacper Faber <kacperf1234@gmail.com>
2023-09-08 12:43:29 +0200

    Created working impl for Index component:
    Will render Index_Header and Index_Content inside indexContainer and index classes.
    
    Besides created Index_Content, already it's empty.

--- src/ui/routes/index/index.tsx
+++ src/ui/routes/index/index.tsx
@@ -1,9 +1,18 @@
 import {AppLayout} from "../../layout/appLayout";
+import {useStyles} from "../../styles/useStyles";
+import {Index_Header} from "./header/indexHeader";
+import {Index_Content} from "./indexContent";
 
 export const Index = () => {
+    const classes = useStyles();
     return (
         <AppLayout>
-            <h1>Hello World!</h1>
+            <div className={classes.indexContainer}>
+                <div className={classes.index}>
+                    <Index_Header/>
+                    <Index_Content/>
+                </div>
+            </div>
         </AppLayout>
     );
 }
---
+++ src/ui/routes/index/indexContent.tsx
@@ -0,0 +1,8 @@
+import {useStyles} from "../../styles/useStyles";
+
+export const Index_Content = () => {
+    const classes = useStyles();
+    return (
+        <div className={classes.indexContent}></div>
+    )
+}
