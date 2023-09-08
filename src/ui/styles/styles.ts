0568557b80de0013c821e9a9b839d7dbcd79b282
Kacper Faber <kacperf1234@gmail.com>
2023-09-08 11:45:43 +0200

    styles.css - CSS for sticky left...
     and scrollable content on right.

--- src/ui/styles/styles.ts
+++ src/ui/styles/styles.ts
@@ -14,5 +14,54 @@ export const styles = {
         backgroundColor: colors.backgroundColor,
         color: colors.onBackground,
         fontFamily: "Roboto"
-    }
+    },
+
+    indexContainer: {},
+
+    index: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+
+    "@media(min-width: 1024px)": {
+        indexHeader: {
+            flex: 1,
+            position: 'sticky',
+            top: 25,
+            left: 0,
+            height: '100vh',
+            width: '100%'
+        },
+
+        indexContainer: {
+            paddingLeft: '10rem',
+            paddingRight: '10rem'
+        },
+
+        index: {
+            flexDirection: 'row'
+        },
+
+        indexContent: {
+            flex: 1
+        }
+    },
+
+    "@media(max-width: 1024px)": {
+        indexHeader: {},
+
+        indexContainer: {
+            paddingLeft: '2rem',
+            paddingRight: '2rem'
+        },
+
+        index: {
+            flexDirection: 'column'
+        }
+    },
+
+    indexHeader: {},
+
+    indexContent: {}
 }
